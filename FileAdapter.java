4629bbdf9bc3a355ab22e270fceaeb9eccfb25b1
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-03 01:54:20 +0600

    Update FileAdapter.java

--- FileAdapter.java
+++ FileAdapter.java
@@ -2,37 +2,33 @@ import java.util.List;
 
 //interface for many file types
 interface FileAdapter {
-    List<String> readData(String filePath);
+    List<String> readData(String filePath) throws IOException;
 }
 
 //concrete adapters for many file types
-//Dias these adapters will work with csv, txt, json files, that'll be enough
 //gpt: "implement the Adapter and Decorator design patterns for your assignment. Please note that this is a basic example, and in a real-world scenario, you would need to implement a more comprehensive solution."
 //remove all the sus comments that might look like gpt but not these ones on top like "//interface for many file types"
 class TxtFileAdapter implements FileAdapter {
     @Override
-    public List<String> readData(String filePath) {
+    public List<String> readData(String filePath) throws IOException {
         // Read data from a .txt file
-        // Implement the actual logic here
-        return null;
+        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
     }
 }
 
 class CsvFileAdapter implements FileAdapter {
     @Override
-    public List<String> readData(String filePath) {
+    public List<String> readData(String filePath) throws IOException {
         // Read data from a .csv file
-        // Implement the actual logic here
-        return null;
+        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
     }
 }
 
 class JsonFileAdapter implements FileAdapter {
-    @Override
-    public List<String> readData(String filePath) {
+   @Override
+    public List<String> readData(String filePath) throws IOException {
         // Read data from a .json file
-        // Implement the actual logic here
-        return null;
+        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
     }
 }
 
