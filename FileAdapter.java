ed4b948207e285d2e19f96ad0e03bab08c98e3e8
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-03 02:50:54 +0600

    Update FileAdapter.java

--- FileAdapter.java
+++ FileAdapter.java
@@ -1,20 +1,26 @@
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-//interface for many file types
+// Interface for many file types
 interface FileAdapter {
     List<String> readData(String filePath) throws IOException;
 }
 
-//concrete adapters for many file types
+// Concrete adapters for many file types
 class TxtFileAdapter implements FileAdapter {
-    @Override
+   @Override
     public List<String> readData(String filePath) throws IOException {
         // Read data from a .txt file
-        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        try {
+            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e; // Re-throw the exception for proper error handling
+        }
     }
 }
 
@@ -22,7 +28,12 @@ class CsvFileAdapter implements FileAdapter {
     @Override
     public List<String> readData(String filePath) throws IOException {
         // Read data from a .csv file
-        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        try {
+            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e; // Re-throw the exception for proper error handling
+        }
     }
 }
 
@@ -30,20 +41,70 @@ class JsonFileAdapter implements FileAdapter {
    @Override
     public List<String> readData(String filePath) throws IOException {
         // Read data from a .json file
-        return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        try {
+            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e; // Re-throw the exception for proper error handling
+        }
     }
 }
 
-//Data aggregation
+// Data aggregation
 class DataAggregator {
     private List<String> aggregatedData;
 
-    public void aggregateData(FileAdapter adapter, String filePath) {
-        // Use the provided adapter to read and aggregate data
-        aggregatedData = adapter.readData(filePath);
+    public void aggregateData(FileAdapter adapter, String filePath) throws IOException {
+        try {
+            // Use the provided adapter to read and aggregate data
+            aggregatedData = adapter.readData(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e; // Re-throw the exception for proper error handling
+        }
     }
 
     public List<String> getAggregatedData() {
         return aggregatedData;
     }
 }
+
+// Main application logic
+// public class Main {
+//     public static void main(String[] args) {
+//         // Sample usage with error handling and user choices
+//         DataAggregator aggregator = new DataAggregator();
+
+//         try {
+//             // User's choices
+//             boolean userChoosesEncryption = true;
+//             boolean userChoosesEncoding = true;
+
+//             // Example usage with TxtFileAdapter for a .txt file
+//             FileAdapter txtAdapter = new TxtFileAdapter();
+//             aggregator.aggregateData(txtAdapter, "sample.txt");
+
+//             List<String> aggregatedData = aggregator.getAggregatedData();
+
+//             // Apply decorators based on user choice
+//             if (userChoosesEncryption) {
+//                 DataDecorator encryptionDecorator = new EncryptionDecorator();
+//                 aggregatedData = encryptionDecorator.decorateData(aggregatedData);
+//             }
+
+//             if (userChoosesEncoding) {
+//                 String encoding = "UTF-16";
+//                 DataDecorator encodingDecorator = new EncodingDecorator(encoding);
+//                 aggregatedData = encodingDecorator.decorateData(aggregatedData);
+//             }
+
+//             // Process the aggregated and decorated data
+//             for (String line : aggregatedData) {
+//                 System.out.println(line);
+//             }
+//         } catch (IOException e) {
+//             // Handle file reading exceptions
+//             System.err.println("An error occurred while reading data from files: " + e.getMessage());
+//         }
+//     }
+// }
