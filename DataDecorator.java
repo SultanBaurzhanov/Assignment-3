1089f2f481b18b60e571319c782eab396c1f5f8a
SultanBaurzhanov <122503241+SultanBaurzhanov@users.noreply.github.com>
2023-11-02 19:27:51 +0500

    Update DataDecorator.java

--- DataDecorator.java
+++ DataDecorator.java
@@ -1,42 +1,60 @@
 import java.util.List;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
-//interface for data decorators
+//decorator pattern
+//interface for data decorations
 interface DataDecorator {
     List<String> decorateData(List<String> data);
 }
 
-//Concrete decorators
+//concrete decorators
+//look up how to encrypt files
 class EncryptionDecorator implements DataDecorator {
+    private /*'final' here if goes wrong*/ String secretKey = "BIGSHOT"; //secret key for encryption
+
+    public EncryptionDecorator(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
     @Override
     public List<String> decorateData(List<String> data) {
-        // Add encryption logic here
-        return data; // Placeholder, implement encryption
+        try {
+            for (int i = 0; i < data.size(); i++) {
+                String originalData = data.get(i);
+                String encryptedData = encrypt(originalData, secretKey);
+                data.set(i, encryptedData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    private String encrypt(String data, String secretKey) throws Exception {
+        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 }
 
 class EncodingDecorator implements DataDecorator {
     @Override
     public List<String> decorateData(List<String> data) {
-        // Add character encoding logic here
-        return data; // Placeholder, implement encoding
+        for (int i = 0; i < data.size(); i++) {
+            String originalData = data.get(i);
+            String encodedData = encode(originalData);
+            data.set(i, encodedData);
+        }
+        return data;
+    }
+
+    private String encode(String data) {
+        byte[] encodedBytes = data.getBytes(StandardCharsets.UTF_8);
+        return Base64.getEncoder().encodeToString(encodedBytes);
     }
 }
-
-//Client code to apply decorators
-DataAggregator aggregator = new DataAggregator();
-
-//Assuming you have already aggregated data from files
-List<String> aggregatedData = aggregator.getAggregatedData();
-
-//Apply decorators based on user choice
-if (userChoosesEncryption) {
-    DataDecorator encryptionDecorator = new EncryptionDecorator();
-    aggregatedData = encryptionDecorator.decorateData(aggregatedData);
-}
-
-if (userChoosesEncoding) {
-    DataDecorator encodingDecorator = new EncodingDecorator();
-    aggregatedData = encodingDecorator.decorateData(aggregatedData);
-}
-
-//Now the aggregated data has been decorated as per user's choice
