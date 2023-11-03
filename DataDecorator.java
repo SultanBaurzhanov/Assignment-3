3a9bb621956f99745e30efbf5b5268a343b2f4d6
SultanBaurzhanov <122503241+SultanBaurzhanov@users.noreply.github.com>
2023-11-03 20:35:47 +0500

    Update DataDecorator.java

--- DataDecorator.java
+++ DataDecorator.java
@@ -4,16 +4,15 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 
-// Decorator pattern
-// Interface for data decorations
+// Decorator Pattern
+// Interface for data decorators
 interface DataDecorator {
     List<String> decorateData(List<String> data);
 }
 
 // Concrete decorators
-// Look up how to encrypt files
 class EncryptionDecorator implements DataDecorator {
-    private /*'final' here if goes wrong*/ String secretKey = "BIGSHOT"; //secret key for encryption
+    private final String secretKey; // Secret key for encryption
 
     public EncryptionDecorator(String secretKey) {
         this.secretKey = secretKey;
@@ -22,6 +21,7 @@ class EncryptionDecorator implements DataDecorator {
     @Override
     public List<String> decorateData(List<String> data) {
         try {
+            // Go through the list of data and encrypt each string
             for (int i = 0; i < data.size(); i++) {
                 String originalData = data.get(i);
                 String encryptedData = encrypt(originalData, secretKey);
@@ -34,9 +34,14 @@ class EncryptionDecorator implements DataDecorator {
     }
 
     private String encrypt(String data, String secretKey) throws Exception {
+        // Create SecretKeySpec from the provided secret key, google for more info if ur not familiar
         SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), "AES");
+
+        // make the AES cipher for encryption
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        // Encrypt the data and encode it in Base64
         byte[] encryptedBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
@@ -45,6 +50,7 @@ class EncryptionDecorator implements DataDecorator {
 class EncodingDecorator implements DataDecorator {
     @Override
     public List<String> decorateData(List<String> data) {
+        // Go through the list of data and apply Base64 encoding to each string
         for (int i = 0; i < data.size(); i++) {
             String originalData = data.get(i);
             String encodedData = encode(originalData);
@@ -54,6 +60,7 @@ class EncodingDecorator implements DataDecorator {
     }
 
     private String encode(String data) {
+        // Encode the data in Base64 format
         byte[] encodedBytes = data.getBytes(StandardCharsets.UTF_8);
         return Base64.getEncoder().encodeToString(encodedBytes);
     }
