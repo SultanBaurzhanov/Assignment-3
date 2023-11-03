6c2f3789b6568b8ef6e749ce0417104697ad787d
SultanBaurzhanov <122503241+SultanBaurzhanov@users.noreply.github.com>
2023-11-03 20:36:02 +0500

    Update FileAdapter.java

--- FileAdapter.java
+++ FileAdapter.java
@@ -1,110 +1,62 @@
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
-// Interface for many file types
+//make FileAdapter interface to read many files
 interface FileAdapter {
-    List<String> readData(String filePath) throws IOException;
+    List<String> readData(String filePath);
 }
 
-// Concrete adapters for many file types
+//Concrete adapters for specific files
 class TxtFileAdapter implements FileAdapter {
-   @Override
-    public List<String> readData(String filePath) throws IOException {
-        // Read data from a .txt file
-        try {
-            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e; // Re-throw the exception for proper error handling
-        }
+    @Override
+    public List<String> readData(String filePath) {
+        // reading a txt
+        System.out.println("Reading data from a .txt file: " + filePath);
+        // make it read the data
+        List<String> data = new ArrayList<>();
+        data.add("Txt data line 1");
+        data.add("Txt data line 2");
+        return data;
     }
 }
 
 class CsvFileAdapter implements FileAdapter {
     @Override
-    public List<String> readData(String filePath) throws IOException {
-        // Read data from a .csv file
-        try {
-            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e; // Re-throw the exception for proper error handling
-        }
+    public List<String> readData(String filePath) {
+        //Implement reading the data from a csv file
+        System.out.println("Reading data from a .csv file: " + filePath);
+        // make it read the data
+        List<String> data = new ArrayList<>();
+        data.add("Csv data line 1");
+        data.add("Csv data line 2");
+        return data;
     }
 }
 
 class JsonFileAdapter implements FileAdapter {
-   @Override
-    public List<String> readData(String filePath) throws IOException {
-        // Read data from a .json file
-        try {
-            return Files.readAllLines(Path.of(filePath), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e; // Re-throw the exception for proper error handling
-        }
+    @Override
+    public List<String> readData(String filePath) {
+        // Implement reading data from a json file
+        System.out.println("Reading data from a .json file: " + filePath);
+        // make it read the data
+        List<String> data = new ArrayList<>();
+        data.add("Json data line 1");
+        data.add("Json data line 2");
+        return data;
     }
 }
 
-// Data aggregation
+// Data Aggregator
 class DataAggregator {
-    private List<String> aggregatedData;
+    private List<String> aggregatedData = new ArrayList<>();
 
-    public void aggregateData(FileAdapter adapter, String filePath) throws IOException {
-        try {
-            // Use the provided adapter to read and aggregate data
-            aggregatedData = adapter.readData(filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e; // Re-throw the exception for proper error handling
-        }
+    public void aggregateData(FileAdapter adapter, String filePath) {
+        //Use the given adapter to read AND agregate data
+        List<String> data = adapter.readData(filePath);
+        aggregatedData.addAll(data);
     }
 
     public List<String> getAggregatedData() {
         return aggregatedData;
     }
 }
-
-// Main application logic
-// public class Main {
-//     public static void main(String[] args) {
-//         // Sample usage with error handling and user choices
-//         DataAggregator aggregator = new DataAggregator();
-
-//         try {
-//             // User's choices
-//             boolean userChoosesEncryption = true;
-//             boolean userChoosesEncoding = true;
-
-//             // Example usage with TxtFileAdapter for a .txt file
-//             FileAdapter txtAdapter = new TxtFileAdapter();
-//             aggregator.aggregateData(txtAdapter, "sample.txt");
-
-//             List<String> aggregatedData = aggregator.getAggregatedData();
-
-//             // Apply decorators based on user choice
-//             if (userChoosesEncryption) {
-//                 DataDecorator encryptionDecorator = new EncryptionDecorator();
-//                 aggregatedData = encryptionDecorator.decorateData(aggregatedData);
-//             }
-
-//             if (userChoosesEncoding) {
-//                 String encoding = "UTF-16";
-//                 DataDecorator encodingDecorator = new EncodingDecorator(encoding);
-//                 aggregatedData = encodingDecorator.decorateData(aggregatedData);
-//             }
-
-//             // Process the aggregated and decorated data
-//             for (String line : aggregatedData) {
-//                 System.out.println(line);
-//             }
-//         } catch (IOException e) {
-//             // Handle file reading exceptions
-//             System.err.println("An error occurred while reading data from files: " + e.getMessage());
-//         }
-//     }
-// }
