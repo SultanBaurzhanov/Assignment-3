d02d36dfb385dae427c767b96e6666144608ca6c
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-08 13:34:53 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,23 +2,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //Create instances of adapters
-        FileAdapter txtAdapter = new TxtFileAdapter();
-        FileAdapter csvAdapter = new CsvFileAdapter();
-        FileAdapter jsonAdapter = new JsonFileAdapter();
+        // Create instances of file parsing strategies
+        FileParsingStrategy txtParsingStrategy = new TxtFileParsingStrategy();
+        FileParsingStrategy csvParsingStrategy = new CsvFileParsingStrategy();
+        FileParsingStrategy jsonParsingStrategy = new JsonFileParsingStrategy();
 
-        //Create a data aggregator
+        // Create a data aggregator
         DataAggregator aggregator = new DataAggregator();
 
-        //Agregate data from many files
-        aggregator.aggregateData(txtAdapter, "test1.txt");
-        aggregator.aggregateData(csvAdapter, "test2.csv");
-        aggregator.aggregateData(jsonAdapter, "test3.json");
+        // Aggregate data from many files using the selected strategy
+        aggregator.aggregateData(txtParsingStrategy, "test1.txt");
+        aggregator.aggregateData(csvParsingStrategy, "test2.csv");
+        aggregator.aggregateData(jsonParsingStrategy, "test3.json");
 
-        //Get agregated data
+        // Get aggregated data
         List<String> aggregatedData = aggregator.getAggregatedData();
 
-        //Print the agregated data for checking
+        // Print the aggregated data for checking
         System.out.println("Aggregated Data:");
         for (String line : aggregatedData) {
             System.out.println(line);
@@ -32,7 +32,7 @@ public class Main {
         DataDecorator encodingDecorator = new EncodingDecorator();
         aggregatedData = encodingDecorator.decorateData(aggregatedData);
 
-        //Create OutputWriter and show the final data
+        // Create OutputWriter and show the final data
         OutputWriter outputWriter = new OutputWriter();
         outputWriter.writeData(aggregatedData);
     }
